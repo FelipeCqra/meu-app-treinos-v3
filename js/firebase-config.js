84c5f0b80c6ef0624ce14a097256ae2b0d021091
FelipeCqra <felipecerqueira.academico@gmail.com>
2026-07-28 19:53:07 -0300

    Update firebase-config.js
    mudanças no código que Trabalhará em conjunto com regras adicionadas no firebase e autenticação com email e senha para maior segurança.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,42 +1,43 @@
-// Importa os módulos necessários do Firebase SDK via CDN oficial
+// 1. Todas as importações no topo
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { 
-    getFirestore, 
-    collection, 
-    addDoc, 
-    getDocs, 
-    doc, 
-    updateDoc, 
-    deleteDoc,
-    query,
-    orderBy
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  updateDoc,
+  deleteDoc,
+  query,
+  orderBy,
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// COLOQUE AS SUAS CREDENCIAIS DO FIREBASE AQUI:
+// 2. Credenciais
 const firebaseConfig = {
-    apiKey: "AIzaSyDVtostrWXcmjLq_wQ-j9MjJ2dlrhPcw2M",
-    authDomain: "gym-tracker-e00bc.firebaseapp.com",
-    projectId: "gym-tracker-e00bc",
-    storageBucket: "gym-tracker-e00bc.firebasestorage.app",
-    messagingSenderId: "406666551868",
-    appId: "1:406666551868:web:edb53f96cc974e93591939"
+  apiKey: "AIzaSyDVtostrWXcmjLq_wQ-j9MjJ2dlrhPcw2M",
+  authDomain: "gym-tracker-e00bc.firebaseapp.com",
+  projectId: "gym-tracker-e00bc",
+  storageBucket: "gym-tracker-e00bc.firebasestorage.app",
+  messagingSenderId: "406666551868",
+  appId: "1:406666551868:web:edb53f96cc974e93591939",
 };
 
-// Inicializa o Firebase
+// 3. Inicializações (Nesta ordem exata)
 const app = initializeApp(firebaseConfig);
-
-// Inicializa o Firestore (Banco de Dados)
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-// Exporta o banco e as funções utilitárias que usaremos nos outros arquivos
-export { 
-    db, 
-    collection, 
-    addDoc, 
-    getDocs, 
-    doc, 
-    updateDoc, 
-    deleteDoc,
-    query,
-    orderBy 
+// 4. Exportações
+export {
+  db,
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  updateDoc,
+  deleteDoc,
+  query,
+  orderBy,
+  auth,
 };
